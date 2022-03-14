2b4deaf41116c89d7c21e061dba97d959694f1d3
Brandon Lin <brandonlin9716@gmail.com>
2022-03-13 18:26:58 -0700

    Unit 1: Pathway 4 - part 5
    Display dice image upon DiceRoll.

--- app/src/main/java/com/example/kotlinjournal/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjournal/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinjournal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     /**
@@ -34,9 +37,20 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         val dice = Dice(6)
-        val resultTextView: TextView = findViewById(R.id.resultTextView)
+        val diceRoll = dice.roll()
 
-        resultTextView.text = dice.roll().toString()
+        val diceImage: ImageView = findViewById(R.id.diceImageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
         Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
     }
 }
