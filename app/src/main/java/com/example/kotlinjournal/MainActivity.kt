c65ffb1030eca04d1d6714d9390eb37f38ed1643
Brandon Lin <brandonlin9716@gmail.com>
2022-03-13 17:59:58 -0700

    Create DiceRoller.kt

--- app/src/main/java/com/example/kotlinjournal/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjournal/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.kotlinjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dice_roller)
 
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * A Dice with a variable number of sides
+     */
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        val dice = Dice(6)
+        val resultTextView: TextView = findViewById(R.id.resultTextView)
+
+        resultTextView.text = dice.roll().toString()
+        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
     }
 }
