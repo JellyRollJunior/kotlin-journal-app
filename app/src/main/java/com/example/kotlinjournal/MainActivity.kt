2baadd48001e2e5271a3fabce23957c6ae197762
Brandon Lin <brandonlin9716@gmail.com>
2022-03-20 19:24:59 -0700

    Unit 2: Pathway 1 - part 4 - Calculate Tip
    Write code for calculating tip.

--- app/src/main/java/com/example/kotlinjournal/MainActivity.kt
+++ app/src/main/java/com/example/kotlinjournal/MainActivity.kt
@@ -7,15 +7,42 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import com.example.kotlinjournal.databinding.ActivityMainBinding
+import com.example.kotlinjournal.databinding.ActivityTipCalculatorBinding
+import java.text.NumberFormat
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityTipCalculatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tip_calculator)
+        binding = ActivityTipCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
-}
+    private fun calculateTip() {
+        val costString = binding.costOfService.text.toString()
+        val cost = costString.toDouble()
 
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when(selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+    }
+}
