2d24ebcba1785e6ce8caa2cfec9a2e7e57e013fc
Brandon Lin <brandonlin9716@gmail.com>
2022-03-20 18:00:36 -0700

    Unit 2: Pathway 1 - part 4 - Build Layout in XML
    Created TipCalculator.kt

--- app/src/main/java/com/example/kotlinjournal/DiceRoller.kt
+++ app/src/main/java/com/example/kotlinjournal/DiceRoller.kt
@@ -2,10 +2,55 @@ package com.example.kotlinjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class DiceRoller : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_roller)
+
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val diceImage: ImageView = findViewById(R.id.diceImageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+    }
+}
+
+/**
+ * A Dice with a variable number of sides
+ */
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
--- app/src/main/java/com/example/kotlinjournal/TipCalculator.kt
+++ app/src/main/java/com/example/kotlinjournal/TipCalculator.kt
@@ -2,20 +2,10 @@ package com.example.kotlinjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
-private const val TAG = "MainActivity"
-
-class MainActivity : AppCompatActivity() {
+class TipCalculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tip_calculator)
-
     }
-
 }
-
