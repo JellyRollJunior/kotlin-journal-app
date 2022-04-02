0105c7be5140a9c57b8ceab1b7f612c89a5bcbdb
Brandon Lin <brandonlin9716@gmail.com>
2022-04-02 16:39:25 -0700

    Move TipCalculator from MainActivity.

--- app/src/main/java/com/example/kotlinjournal/TipCalculator.kt
+++ app/src/main/java/com/example/kotlinjournal/TipCalculator.kt
@@ -1,11 +1,64 @@
 package com.example.kotlinjournal
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.kotlinjournal.databinding.ActivityTipCalculatorBinding
+import java.text.NumberFormat
 
 class TipCalculator : AppCompatActivity() {
+    private lateinit var binding: ActivityTipCalculatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tip_calculator)
+        binding = ActivityTipCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip()}
+
+        binding.costOfService.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+
+        displayTip(0.0)
+    }
+
+    private fun calculateTip() {
+        val costString = binding.costOfServiceEditText.text.toString()
+        val cost = costString.toDoubleOrNull()
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
